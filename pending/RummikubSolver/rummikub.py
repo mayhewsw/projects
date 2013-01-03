0d779f2694ba501e5970951345dbcb62a691ebed
Stephen Mayhew <swm.mayhew@gmail.com>
2013-01-03 09:30:23 -0600

    Added BoggleSolver

--- pending/RummikubSolver/rummikub.py
+++ pending/RummikubSolver/rummikub.py
@@ -17,18 +17,23 @@ import random
 # and must, at each turn, choose to play
 # or if that is not possible, draw from the table
 
+_sep = ":"
+colors = ["black", "blue", "yellow", "red"]
+
+
 def getTiles():
     ''' this creates the board. There are 106 tiles, this creates a
     list with a certain string representing each tile '''
+
+    mycolors = map(lambda s: s + _sep, colors)
     
-    colors = ["black:", "blue:", "yellow:", "red:"]
     tiles = []
     for t in range(1,14):
-        four_tiles = map(lambda c: c+str(t),colors)
+        four_tiles = map(lambda c: c+str(t),mycolors)
         tiles.extend(four_tiles) # we do this twice
         tiles.extend(four_tiles)
-    tiles.append("red:joker")
-    tiles.append("black:joker")
+    tiles.append("red:-5")
+    tiles.append("black:-5")
     return tiles
 
 
@@ -40,40 +45,115 @@ class Player:
     def __init__(self, playerNumber):
         self.num = playerNumber
         self.started = False
-        self.tiles = []
+        # tiles is dictionary of form:
+        #   {color : [num, num, num], ...}
+        self.tiles = {}
+        for c in colors:
+            self.tiles[c] = []
 
     def __repr__(self):
         return "P" + str(self.num) + ": " + str(self.tiles)
 
+    def getNumTiles(self):
+        return sum(map(len, self.tiles.values()))
+    
     def play(self):
         ''' if can play, make the play (modify the board) and return true
         otherwise, return false '''
         global board
-        if p.started:
+        if self.started:
             # normal move code goes here
             # examine the board
-            pass
+            canMove = False
+            if canMove:
+                # then move!
+                return True
+            else:
+                return False
+                
         else:
             # move has to be above 30
             # cannot use tiles from the board
             canMove = False
-            
-            if canMove:
-                # move! then...
-                self.started = True
+
+            plays = []
+            for c in self.tiles:
+                nums = self.tiles[c]
+                # are there three consecutive
+                cplays = getAllSequences(nums)
+                fplays = []
+                for p in cplays:
+                    fplays.append(map(lambda n: c + _sep + str(n), p))
+                plays.extend(fplays)
+            # now check the plays...
+            # is the sum greater than 30?
+
+            for i in range(1, 14):
+                # do all four colors contain i?
+                t = map(lambda lst: i in lst, self.tiles.values())
+                # are there 3 or more trues in t?
+                if sum(t) >= 3:
+                    play = []
+                    for color, val in zip(self.tiles.keys(), t):
+                        if val:
+                            play.append(color + _sep + str(i))
+                    plays.append(play)
+
+            # FIXME: need to check for conflicts...
+
+            # get the sum of all values in plays
+            total = 0
+            for play in plays:
+                total += sum(map(lambda s: int(s.split(_sep)[1]), play))
+
+            if total >= 30:
+                # change the board
+                return True
             else:
-                self.draw(1)
-            
-    def draw(self, n):
+                return False
+                
+    def draw(self, n, l=[]):
         ''' draw n tiles from gameTiles '''
         global gameTiles
-        
-        newtiles = random.sample(gameTiles, n)
-        self.tiles.extend(newtiles)
+
+        # get the tiles
+        if len(l) == 0:
+            newtiles = random.sample(gameTiles, n)
+        else:
+            newtiles = l
+
+        # remove tiles from remaining
+        # add the new tiles to the hand
         for t in newtiles:
             gameTiles.remove(t)
-        
+            color, num = t.split(_sep)
+            self.tiles[color].append(int(num))
 
+        # sort the tiles in each color
+        for t in self.tiles:
+            self.tiles[t] = sorted(self.tiles[t])
+
+def getAllSequences(nums):
+    ''' given a (sorted) list of numbers, this will
+    find all consecutive sequences of length 3
+    or greater, and return a list of those sequences '''
+    if len(nums) == 0:
+        return []
+    plays = []
+    play = [nums[0]]
+    for n in nums[1:]:
+        if n == play[-1] + 1:
+            play.append(n)
+        else:
+            if len(play) >= 3:
+                plays.append(play)
+            play = [n]
+    if len(play) >= 3:
+        plays.append(play)
+    return plays
+
+            
+            
 def mainGameLoop():
     # define the # of players
     numPlayers = 4
@@ -81,8 +161,8 @@ def mainGameLoop():
     # create players and deal
     players = []
     for i in range(numPlayers):
-        pTiles = random.sample(gameTiles,14)
-        p = Player(i+1, pTiles)
+        p = Player(i+1)
+        p.draw(14)
         print p
         players.append(p)
 
--- pending/RummikubSolver/rummikubTest.py
+++ pending/RummikubSolver/rummikubTest.py
@@ -7,37 +7,73 @@ class TestRummikub(unittest.TestCase):
     def setUp(self):
         self.board = []
         self.p = rummikub.Player(1)
+        rummikub.gameTiles = rummikub.getTiles()
+
 
     def test_init(self):
-        rummikub.gameTiles = rummikub.getTiles()
         self.p.draw(14)
 
-        self.assertEqual(len(self.p.tiles), 14)
+        self.assertEqual(self.p.getNumTiles(), 14)
+        # self.assertEqual(sum(self.p.tiles.values()), 14)
         self.assertEqual(len(rummikub.gameTiles), 106 - 14)
+
+    def test_len(self):
+        self.assertEqual(self.p.getNumTiles(), 0)
         
+    def test_draw(self):
+        self.p.draw(1, ["blue:12", "red:1", "blue:3", "yellow:8","yellow:1", "yellow:3"])
 
-    def test_start(self):
-        # give the player a set of tiles
-        tiles = ["red:9", "red:10", "red:11"]
-        self.p.tiles = tiles
-        self.assertTrue(self.p.canStart())
-        self.assertTrue(self.p.started)
+        self.assertEqual(self.p.getNumTiles(), 6)
+        self.assertEqual(self.p.tiles["yellow"][0], 1)
+        self.assertEqual(self.p.tiles["yellow"][1], 3)
+        self.assertEqual(self.p.tiles["yellow"][2], 8)
 
-        tiles = ["red:11", "blue:11", "yellow:11"]
-        self.p.tiles = tiles
-        self.assertTrue(self.p.canStart())
-        self.assertTrue(self.p.started)
+        self.p.draw(1, ["blue:12"])
+        self.assertEqual(self.p.getNumTiles(), 7)
+        self.assertEqual(self.p.tiles["blue"][0], 3)
+        self.assertEqual(self.p.tiles["blue"][1], 12)
+        self.assertEqual(self.p.tiles["blue"][2], 12)
         
-        tiles = ["red:9", "blue:9", "yellow:9"]
-        self.p.tiles = tiles
-        self.assertFalse(self.p.canStart())
-        self.assertFalse(self.p.started)
+    def test_true(self):
+        self.p.draw(0, ["blue:12", "red:1", "blue:11", "yellow:8","blue:10"])
+        v = self.p.play()
+        self.assertTrue(v)
+        # board has changed?
 
-        tiles = ["red:12", "blue:12", "yellow:11"]
-        self.p.tiles = tiles
-        self.assertFalse(self.p.canStart())
-        self.assertFalse(self.p.started)
+    def test_true2(self):
+        self.p.draw(0, ["blue:10", "red:10", "yellow:10"])
+        v = self.p.play()
+        self.assertTrue(v)
+        # board has changed?
+        
+    def test_false(self):
+        self.p.draw(0, ["blue:1", "red:1", "blue:11", "yellow:8","yellow:10"])
+        v = self.p.play()
+        self.assertFalse(v)
+        # board hasn't changed
 
+    def test_false2(self):
+        self.p.draw(0, ["blue:1", "red:1", "blue:2", "blue:3","yellow:10"])
+        v = self.p.play()
+        self.assertFalse(v)
+        # board hasn't changed
+
+    def test_seqs(self):
+        seq = [1, 2, 3]
+        self.assertEqual([seq], rummikub.getAllSequences(seq))
+
+        in_seq = [1, 3, 6]
+        self.assertEqual([], rummikub.getAllSequences(in_seq))
+
+        in_seq = [1, 2, 3, 7, 8, 9, 10]
+        out_seq = [[1, 2, 3], [7, 8, 9, 10]]
+        self.assertEqual(out_seq, rummikub.getAllSequences(in_seq))
+        
+        in_seq = [1, 2, 3,4, 7, 8]
+        out_seq = [[1, 2, 3,4]]
+        self.assertEqual(out_seq, rummikub.getAllSequences(in_seq))
+
+        
         
     def test_play(self):
         lenptiles = len(self.p.tiles)
