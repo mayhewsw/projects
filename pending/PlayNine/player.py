86c92ef385979f5721057759de96bec3fa014bfa
Stephen Mayhew <swm.mayhew@gmail.com>
2013-12-22 21:18:13 -0600

    Updated playnine game

--- pending/PlayNine/player.py
+++ pending/PlayNine/player.py
@@ -1,13 +1,196 @@
 # Create all the players necessary
+from abc import ABCMeta, abstractmethod
+import logging
+FORMAT = "[%(asctime)s] : %(filename)s.%(funcName)s():%(lineno)d - %(message)s"
+DATEFMT = '%H:%M:%S, %m/%d/%Y'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt=DATEFMT)
+logger = logging.getLogger(__name__)
+import random
 
-def Player():
 
-    def __init__():
-        hand = [(-1, -1) for i in range(8)]
+class Player():
+    __metaclass__ = ABCMeta
 
+    # this number needs to be even
+    NUM_CARDS = 8
 
-def RandomPlayer(Player):
+    def __init__(self, num, board):
+        # initialize cards from deck
+        self.num = num
+        self.board = board
 
-    def choosePile():
+        self.mycards = []
+        self.cardisvisible = [["?", "?"] for i in range(0, self.NUM_CARDS, 2)]
+
+    def __str__(self):
+        return "[{} {}]".format(self.__class__.__name__, self.num)
+
+    def setmycards(self, cards):
+        if len(cards) != self.NUM_CARDS:
+            logger.error("Num cards needs to be {}, but is {}".format(self.NUM_CARDS, len(cards)))
+            return
+        self.mycards = [[cards[i], cards[i+1]] for i in range(0, self.NUM_CARDS, 2)]
+        self.flip2()
+
+    def flip2(self):
+        """
+        set two cards to be face up.
+        """
+        r1 = (random.randint(0, self.NUM_CARDS/2-1), random.randint(0,1))
+        r2 = (random.randint(0, self.NUM_CARDS/2-1), random.randint(0,1))
+
+        while r1 == r2:
+            r2 = (random.randint(0, self.NUM_CARDS/2-1), random.randint(0,1))
+
+        self.cardisvisible[r1[0]][r1[1]] = self.mycards[r1[0]][r1[1]]
+        self.cardisvisible[r2[0]][r2[1]] = self.mycards[r2[0]][r2[1]]
+
+    def score(self):
+        """ This will score the board of this player at the end of the game, or before
+        """
+        # 1 pairs: 0 points
+        # 2 pairs: -10
+        # 3 pairs: -15
+        # score -5
+
+        from collections import defaultdict
+        pairs = defaultdict(int)
+
+        # otherwise, get the sum
+        score = 0
+        for c1, c2 in self.mycards:
+            #print(c1, c2)
+            if c1 != c2 or c1 == -5 or c2 == -5:
+                score += c1 + c2
+            else:
+                # they are the same
+                pairs[c1] += 1
+        for k in pairs:
+            t = pairs[k]
+            if t == 2:
+                score -= 10
+            if t == 3:
+                score -= 15
+
+        return score
+
+    def isdone(self):
+        """
+        Check if this player is done. (If any of "?" remain in cardisvisible, then NO)
+        """
+        return not "?" in sum(self.cardisvisible, [])
+
+    @abstractmethod
+    def maketurn(self):
         pass
 
+
+class RandomPlayer(Player):
+    """ This player chooses randonly from the different options.
+    """
+
+    def maketurn(self):
+        """
+        Do a turn
+        """
+        #logger.debug("player {} takes turn".format(self.num))
+        #logger.debug(self.mycards)
+        #logger.debug(self.cardisvisible)
+
+        # dumb player: always choose from the draw pile
+        drawn = self.board.drawpile[0]
+        del self.board.drawpile[0]
+
+        # find a location that is unseen, and replace it.
+        for i, v in enumerate(self.cardisvisible):
+            if v[0] == "?":
+                # use this
+                self.board.discardpile.insert(0, self.mycards[i][0])
+                self.mycards[i][0] = drawn
+                v[0] = drawn
+                return
+            elif v[1] == "?":
+                # use this
+                self.board.discardpile.insert(0, self.mycards[i][1])
+                self.mycards[i][1] = drawn
+                v[1] = drawn
+                return
+
+
+class SlightlyBetterPlayer(Player):
+    """ This player chooses randonly from the different options.
+    """
+
+    def maketurn(self):
+        """
+        Do a turn
+        """
+
+        # first check if we have a match for the discard
+        discard = self.board.discardpile[0]
+        for v, card in zip(self.cardisvisible, self.mycards):
+            if card[1] == discard and card[0] != discard:
+                # replace card[0]
+                self.board.discardpile.insert(0, card[0])
+                card[0] = discard
+                v[0] = discard
+                return
+            elif card[0] == discard and card[1] != discard:
+                # replace card[1]
+                self.board.discardpile.insert(0, card[1])
+                card[1] = discard
+                v[1] = discard
+                return
+
+        # dumb player: always choose from the draw pile
+        drawn = self.board.drawpile[0]
+        del self.board.drawpile[0]
+
+        # if this is -5, replace the largest card that is not a pair
+        if drawn == -5:
+            for v, card in zip(self.cardisvisible, self.mycards):
+                if v[0] == "?":
+                    self.board.discardpile.insert(0, drawn)
+                    v[0] = card[0]
+                    return
+                elif v[1] == "?":
+                    self.board.discardpile.insert(0, drawn)
+                    v[1] = card[1]
+                    return
+
+        # if this card can make a pair then keep it and return
+        for v, card in zip(self.cardisvisible, self.mycards):
+            if card[1] == drawn and card[0] != drawn:
+                # replace card[0]
+                self.board.discardpile.insert(0, card[0])
+                card[0] = drawn
+                v[0] = drawn
+                return
+            elif card[0] == drawn and card[1] != drawn:
+                # replace card[1]
+                self.board.discardpile.insert(0, card[1])
+                card[1] = drawn
+                v[1] = drawn
+                return
+
+        total = 0
+        for c in sum(self.cardisvisible,  []):
+            if c == "?":
+                total += 1
+
+        if total > 1:
+            # just open up the first unopened one
+            for v, card in zip(self.cardisvisible, self.mycards):
+                if v[0] == "?":
+                    self.board.discardpile.insert(0, drawn)
+                    v[0] = card[0]
+                    return
+                elif v[1] == "?":
+                    self.board.discardpile.insert(0, drawn)
+                    v[1] = card[1]
+                    return
+
+
+
+if __name__ == "__main__":
+    pass
--- pending/PlayNine/playnine.py
+++ pending/PlayNine/playnine.py
@@ -1,22 +1,152 @@
 #!/usr/bin/python3
 # Play Nine, programmed in Python
 
-# We need a board class
+# how many cards:
+# 8 of each card
+# 0-12
+# -5
+
+from player import RandomPlayer, Player, SlightlyBetterPlayer
+from random import shuffle
+import logging
+FORMAT = "[%(asctime)s] : %(filename)s.%(funcName)s():%(lineno)d - %(message)s"
+DATEFMT = '%H:%M:%S, %m/%d/%Y'
+logging.basicConfig(level=logging.DEBUG, format=FORMAT, datefmt=DATEFMT)
+logger = logging.getLogger(__name__)
+import sys
+
 
 class Board():
+    """ We need a board class """
+
+    MAX_PLAYERS = 6
 
     def __init__(self):
-        unseenCards = {}
-        unseenCards = {r : 8 for r in range(13)}
-        unseenCards[-5] = 8
-        
-        print(unseenCards)
-        #players = [RandomPlayer, RandomPlayer, RandomPlayer, RandomPlayer]
+        self.drawpile = []
 
-    def getAverageUnseenValue(self):
-        pass
+        for i in range(13):
+            self.drawpile.extend([i]*Player.NUM_CARDS)
 
+        self.drawpile.extend([-5]*Player.NUM_CARDS)
+
+        shuffle(self.drawpile)
+
+        self.discardpile = [self.drawpile[0]]
+        del self.drawpile[0]
+        self.players = []
+
+    def addplayer(self, player):
+        if len(self.players) == self.MAX_PLAYERS:
+            logger.error("Cannot add a player! Only allow {} players".format(self.MAX_PLAYERS))
+            sys.exit(-1)
+
+        self.players.append(player)
+
+    def deal(self):
+        for player in self.players:
+            player.setmycards(self.drawpile[:Player.NUM_CARDS])
+            # now remove them from the drawpile
+            self.drawpile = self.drawpile[Player.NUM_CARDS:]
+
+    def rungame(self, num=0):
+        if num % 500 == 0:
+            logger.debug("Run game : {}".format(num))
+
+        firsttogoout = ""
+        checkdone = True
+
+        if len(self.players) == 0:
+            logger.error("Cannot play with 0 players!")
+            return
+
+        totalturns = 0
+        while True:
+            for player in self.players:
+                if firsttogoout == player:
+                    #logger.debug("END OF GAME!")
+                    break
+                player.maketurn()
+                totalturns += 1
+
+                logger.debug(len(self.drawpile))
+                if len(self.drawpile) == 0:
+                    logger.debug("0 len drawpile!!!")
+                    sys.exit(-1)
+
+                if player.isdone() and checkdone:
+                    #logger.debug(str(player) + " has finished")
+                    firsttogoout = player
+                    checkdone = False
+            else:
+                continue
+            break
+
+        minscore = 200000
+        minp = None
+
+        #logger.debug("Total turns: {}".format(totalturns))
+
+        for p in self.players:
+            s = p.score()
+            #print("{} : {}".format(p, s))
+            if s < minscore:
+                minscore = s
+                minp = p
+        #logger.debug("Winner is: {} with score: {}".format(minp, minscore))
+        return minscore, minp
+
+
+def runmanygames():
+    from collections import defaultdict
+
+    winners = defaultdict(int)
+    winnerscores = defaultdict(int)
+
+    for j in range(10000):
+        b = Board()
+
+
+        b.addplayer(RandomPlayer(1, b))
+        b.addplayer(RandomPlayer(2, b))
+        b.addplayer(SlightlyBetterPlayer(3, b))
+        b.addplayer(SlightlyBetterPlayer(4, b))
+
+        b.deal()
+
+        minscore, minp = b.rungame(j)
+
+        winners[str(minp)] += 1
+        winnerscores[str(minp)] += minscore
+
+    print("{:<30} {:<15} {:<15} {:<15}".format("Player", "Games Won", "Cum. Score", "Avg. Score"))
+    print("=====================================================================================")
+    for k in sorted(winners):
+        print("{:<30} {:<15} {:<15} {:<15}".format(k, winners[k], winnerscores[k], winnerscores[k] / float(winners[k])))
+
+    #print(winners)
+    #print(winnerscores)
+
+
+def runsinglegame():
+    b = Board()
+
+    #b.addplayer(RandomPlayer(1, b))
+    #b.addplayer(RandomPlayer(2, b))
+    b.addplayer(SlightlyBetterPlayer(1, b))
+    b.addplayer(SlightlyBetterPlayer(2, b))
+    b.addplayer(SlightlyBetterPlayer(3, b))
+    b.addplayer(SlightlyBetterPlayer(4, b))
+
+    b.deal()
+
+    minscore, minp = b.rungame(1)
+
+    #for p in b.players:
+    #    print(p.mycards)
+    #    print(p.cardisvisible)
+
+    print(minscore, str(minp))
 
 if __name__ == "__main__":
-    b = Board()
-    
+    #runsinglegame()
+    runmanygames()
